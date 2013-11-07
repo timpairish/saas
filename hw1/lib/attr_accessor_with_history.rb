c757618ddf78cb4f307d22702e60e1f4d6d00e7c
Tim <timpairish@gmail.com>
2013-11-06 17:50:34 -0800

    hw1 metaprogramming

--- hw1/lib/attr_accessor_with_history.rb
+++ hw1/lib/attr_accessor_with_history.rb
@@ -3,8 +3,16 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval %Q{
-      # YOUR CODE HERE
-    }
+
+    define_method("#{attr_name}_history") do
+      log = instance_variable_get("@#{attr_name}_log")
+      log[0..-2] if log
+    end
+
+    define_method("#{attr_name}=") do |value|
+      log = instance_variable_get("@#{attr_name}_log") || [nil]
+      instance_variable_set "@#{attr_name}_log", log << value
+      instance_variable_set "@#{attr_name}", value
+    end
   end
 end
